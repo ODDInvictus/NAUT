92bea00d32728bb1e2330b188dd76b063cc43350
Niels <git@nierot.com>
2024-01-01 19:04:55 +0100

    components: Add title

---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let title: string
+</script>
+
+<div class="container">
+  <h1>{title}</h1>
+
+  <hr />
+</div>
+
+<style lang="sass">
+  .container
+    margin-top: 1rem
+
+  hr
+    margin-right: 1rem
+</style>
