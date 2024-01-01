b979c0c0f1777254d65cfca5b8c94347dbe851f7
Niels <git@nierot.com>
2024-01-01 21:30:44 +0100

    components: Add `<title>` to `<Title>`

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
   export let title: string
+
+  export let showTitle: boolean = true
 </script>
+
+<svelte:head>
+  {#if showTitle}
+    <title>{title} - N.A.U.T</title>
+  {:else}
+    <title>N.A.U.T</title>
+  {/if}
+</svelte:head>
 
 <div class="container">
   <h1>{title}</h1>
