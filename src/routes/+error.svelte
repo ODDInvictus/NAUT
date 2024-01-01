431650df373249503a544d0c4fdd0f4fda5c6784
Niels <git@nierot.com>
2024-01-01 19:05:31 +0100

    feat: Add `+error.svelte`

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<main class="container">
+  <h1>{$page.status}</h1>
+  <p>{$page.error?.message}</p>
+</main>
+
+<style lang="sass">
+  main
+    height: 100%
+    width: 100%
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+
+</style>
