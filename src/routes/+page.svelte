f563552b495bc788a2d272cbb27fb08d1dde9765
Niels <git@nierot.com>
2024-01-01 21:31:42 +0100

    page: Add homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
-<div>
-  Dit is een container
+<script lang="ts">
+  import Title from '$lib/components/Title.svelte'
+  import type { PageData } from './$types'
+
+  export let data: PageData
+</script>
+
+<div class="container">
+  <Title title="Goedemiddag, {data.user.name}" showTitle={false} />
 </div>
+
+<style lang="sass">
+  .container
+    margin-top: 1rem
+</style>
